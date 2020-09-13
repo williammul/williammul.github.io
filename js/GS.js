0669239f7721c0d4d5d6a7a209925af32f2b9495
williamm <46639588+williammul@users.noreply.github.com>
2020-09-13 19:34:25 +1000

    common navigation bar
    added common navigation bar

---
+++ js/GS.js
@@ -0,0 +1,8 @@
+  $(document).ready(function () {
+	  $("#nav-placeholder").load("/nav.html");
+       /* var url = window.location;
+        $('a.nav-item[href="'+ url +'"]').parent().addClass('active');
+        $('a.nav-item').filter(function() {
+             return this.href == url;
+        }).parent().addClass('active');*/
+});
