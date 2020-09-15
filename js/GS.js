beb53643ceea419f0616b83c2812e7b3d00eed66
williamm <46639588+williammul@users.noreply.github.com>
2020-09-15 19:11:41 +1000

    updated JS

--- js/GS.js
+++ js/GS.js
@@ -1,8 +1,26 @@
   $(document).ready(function () {
-	  $("#nav-placeholder").load("/nav.html");
+	  loadnav();
+	  activatenagivation();
+	  
        /* var url = window.location;
         $('a.nav-item[href="'+ url +'"]').parent().addClass('active');
         $('a.nav-item').filter(function() {
              return this.href == url;
         }).parent().addClass('active');*/
 });
+function loadnav() {
+	$("#nav-placeholder").load("/nav.html");
+	$(".navbar").unwrap();
+}
+
+function activatenavigation(){
+	let currentHtml = window.location.href.split("/")[window.location.href.split("/").length-1];
+	let navlinks = [];
+	navlinks = document.getElementsByClassName("nav-link");
+	for (let i in navlinks){
+		if(navlinks[0].href = currentHTML){
+			navlinks.classList.add("active");
+		}
+	}
+};
+	
