0e48d21cac442632e99ed37f769d3d840f85e090
williamm <46639588+williammul@users.noreply.github.com>
2020-10-08 20:33:14 +1100

    Update GS.js

--- js/GS.js
+++ js/GS.js
@@ -45,7 +45,7 @@ function searchGBF(){
 					$.each(value, function(key2,value2) {
 						console.log("Key2 is : " +key2);
 						$.each(selected_tags, function(tagitem){
-								if($.inArray(tagitem,value2.Tags)){
+								if($.inArray(tagitem,value2.Tags != -1)){
 								console.log(value2.Tags);
 								result.push(value2.id);
 								console.log(result);	
