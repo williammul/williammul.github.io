8f70f8aa7569667837e92a78c3ba250c6aafc601
williamm <46639588+williammul@users.noreply.github.com>
2020-10-08 19:54:16 +1100

    Update GS.js

--- js/GS.js
+++ js/GS.js
@@ -44,6 +44,7 @@ function searchGBF(){
 				if(selectedtype == key){
 					$.each(value, function(key2,value2) {
 						console.log("Key2 is : " +key2);
+						console.log("value2.Tags);
 						if(key2 == "Tags"){
 						console.log(value2.Tags)	
 						console.log(result);
